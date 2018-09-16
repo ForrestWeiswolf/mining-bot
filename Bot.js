22be828daad78b9a042c8ee353b4eb3de7ba5283
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-09-16 16:05:22 -0400

    Bot.move checks that the coordinates are in bounds

--- Bot.js
+++ Bot.js
@@ -1,4 +1,4 @@
-const { register, move, scan, claim, release, mine } = require('./actions.js')
+const { register, move, scan, claim, release, mine } = require('./endpoints.js')
 
 function Bot(callsign) {
   this.callsign = callsign
@@ -14,9 +14,19 @@ Bot.prototype.register = function() {
 // moves relative to current position - e.g. bot.move(-1, 0) moves the bot
 // one space in the -X direction
 Bot.prototype.move = function(x, y) {
-  return move(this.callsign, this.loc.X + x, this.loc.Y + y).then(loc => {
-    this.loc = loc
-  })
+  const destination = { x: this.loc.X + x, y: this.loc.Y + y }
+  if (
+    destination.x >= 100 ||
+    destination.x < 0 ||
+    destination.y >= 100 ||
+    destination.y < 0
+  ) {
+    throw new Error('Coordinate out of bounds')
+  } else {
+    return move(this.callsign, this.loc.X + x, this.loc.Y + y).then(loc => {
+      this.loc = loc
+    })
+  }
 }
 
 Bot.prototype.scan = function() {
